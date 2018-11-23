9d3ec1b38c772a8e64a6bb5b0e8a530214173598
Sarah Yun <sarahyun7@gmail.com>
2018-11-23 20:15:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,15 +27,16 @@ function addToCart(item) {
 
 
 function viewCart() {
-  if (cart.length > 0) {
-    for (let i = 0; i < cart.length; i++) {
-      
-    }
-  } else if (cart.length === 1) {
+  if (cart.length === 1) {
     return `In your cart, you have ${item} at $${price}.`;
   
   } else if (cart.length === 2) {
     return  
+    
+  } else if (cart.length > 0) {
+    for (let i = 0; i < cart.length; i++) {
+      
+    }
     
   } else if (cart.length === 0) {
     return 'Your shopping cart is empty.';
