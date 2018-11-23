e3e62f50d046df6eef8436a76b7de6abf598fb1c
Sarah Yun <sarahyun7@gmail.com>
2018-11-23 20:44:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,9 +29,6 @@ addToCart('bananas');
 
 function viewCart() {
   
-  var list = [];
-  list.push(item)
-  
   if (cart.length === 1) {
     return `In your cart, you have ${item} at $${price}.`;
   
